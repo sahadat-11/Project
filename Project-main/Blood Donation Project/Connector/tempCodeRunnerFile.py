54643d7444f9100c2a664298cc4bcbb1f3a30a39
sahadat-11 <sahadatfahim999@gmail.com>
2025-07-22 11:04:35 +0600

    add new
    add

--- Project-main/Blood Donation Project/Connector/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-@app.route("/add-user", methods=["POST"])
-def add_student():
-    img=request.files['image']
-    final=final_dict(request=request)
-    res=a_user(connector,final["main"])
-    if res.status=="201 CREATED" and img.filename!="":
-        img.save(final["file"])
-    return res
